568c6afbd7f819b033128b8e497acf81b31e58cf
Martin Kuchar <kucharmartin0@gmail.com>
2020-04-21 14:54:46 -0700

    Finnish For in loop

---
+++ Lab/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+ ## Exercise - For-In Loops
+ 
+ Create a for-in loop that loops through values 1 to 100, and prints each of the values.
+ */
+//for i in 1...100 {
+//    print(i)
+//}
+/*:
+ Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
+ */
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in 0...alphabet.count - 1 {
+    print(alphabet[alphabet.index(alphabet.startIndex, offsetBy: i)])
+}
+
+/*:
+ Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
+ */
+var cap: [String: String]
+cap = ["Slovakia": "Bratislava", "Czechia": "Prague", "Germany": "Berlin"]
+for (key, value) in cap {
+    print("\(key) \(value)")
+}
+
+//: page 1 of 6  |  [Next: App Exercise - Movements](@next)
