d540c959d4dd482cfb0fe7b72dcf73d4ac402f7d
Martin Kuchar <kucharmartin0@gmail.com>
2020-04-22 15:33:24 -0700

    Lab - Tutorial Screen

--- Lab/Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
+++ Lab/Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
@@ -27,12 +27,12 @@ print(food(meal: "Lunch"))
  
  Write a function that will check to see if your meal log (a dictionary like that in the previous exercise) is saved to the device. If it is, return the meal log. If it isn't, return an empty dictionary of type `[String: Any]`. The code you should use in this exercise for retrieving something saved to the device is `UserDefaults.standard.dictionary(forKey: "mealLog")`. This code will return an optional `[String: Any]`. If it returns a value, that is your meal log. If it returns `nil`, then no meal log has been saved. Call the function and print the return value.
  */
-UserDefaults.standard.dictionary(forKey: "mealLog")
+
 func mealy(meal: String) -> [String: Any]? {
    if let food = UserDefaults.standard.dictionary(forKey: meal) {
         return food
     }
     return nil
 }
-print(mealy(meal: "Lunch"))
+print(mealy(meal: "mealLog"))
 //: [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Failable Initializers](@next)
---
+++ Lab/Lab - Tutorial Screen/Lab - Tutorial Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab - Tutorial Screen
+//
+//  Created by Martin Kuchar on 2020-04-22.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
