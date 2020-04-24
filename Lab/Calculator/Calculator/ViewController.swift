2bd9c104b0fb4cccc5f9eea3c46587f93c0e3718
Martin Kuchar <kucharmartin0@gmail.com>
2020-04-23 18:22:36 -0700

    Begin Calculator project

---
+++ Lab/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Martin Kuchar on 2020-04-23.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
