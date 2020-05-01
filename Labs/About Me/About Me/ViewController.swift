231625961ca5554d36a6d5a06c8ed293516f71f8
Martin Kuchar <kucharmartin0@gmail.com>
2020-04-30 18:43:29 -0700

    Add about me lab

---
+++ Labs/About Me/About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Martin Kuchar on 2020-04-30.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
