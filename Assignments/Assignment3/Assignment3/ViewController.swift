7666242fcc4f75c50b460e9c6a0bc9ce3d8c97e6
Martin Kuchar <kucharmartin0@gmail.com>
2020-05-01 15:31:06 -0700

    Finnish Assignment3

---
+++ Assignments/Assignment3/Assignment3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Martin Kuchar on 2020-05-01.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindToOne(_ sender: UIStoryboardSegue){}
+}
